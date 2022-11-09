91f538b889739ca938c11de8ca1116f6f6b1fb52
fifitt <tiarabintang@gmail.com>
2022-11-09 08:55:22 +0700

    first commit

---
+++ app/src/main/java/com/example/fifitjobsheet18/layout_header_drawer.kt
@@ -0,0 +1,11 @@
+package com.example.fifitjobsheet18
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class layout_header_drawer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_header_drawer)
+    }
+}
